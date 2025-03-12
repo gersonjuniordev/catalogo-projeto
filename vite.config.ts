d0d38cdeb68509110e230fe8c2589d9b83de2b6d
gersonjuniordev <gersonjunior125@gmail.com>
2025-03-12 12:31:42 -0300

    att

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { dirname, resolve } from 'path'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
+// https://vitejs.dev/config/
 export default defineConfig({
   base: '/catalogo-projeto/',
   plugins: [react()],
